24c051124ce470a4a7f030c3d1994ab9417300f0
Kee-Yip Chan <keeyip@huddler-inc.com>
2011-08-20 12:51:31 -0700

    Experimenting with a different whitespace style

--- _lib/tinyTinyFw.layouts.php
+++ _lib/tinyTinyFw.layouts.php
@@ -1,17 +1,18 @@
 <?php
 
 function layout2columns($controller) {
-	echo dom('table',
+	echo
+	dom('table',
 		array_map(function($field) {
 			$domLabel = $field->config['display:title'];
 			if (is_string($domLabel))
 				$domLabel = dom('label', $domLabel);
 
 			return
-				dom('tr',
-					dom('th', $domLabel),
-					dom('td',
-						dom('input')));
+			dom('tr',
+				dom('th', $domLabel),
+				dom('td',
+					dom('input')));
 		}, $controller->fields)
 	);
 }
